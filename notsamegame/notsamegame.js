5ade0fe89094fbc46bb7239443fe01b0a35c48be
Alejandro Echeverria <alejandro@echev.com>
2014-11-14 20:25:55 -0300

    Notsamegame proc tool finished

--- notsamegame/notsamegame.js
+++ notsamegame/notsamegame.js
@@ -3,49 +3,46 @@ var currentContext;
 var width, height; 
 
 var grid;
-var tileSize = 50;
-var numRows = 50;
-var numColumns = 150;
+var tileWidth = 15;
+var tileHeight = 15;
+var numRows = 25;
+var numColumns = 40;
 var numberOfColors = 3;
  
-var colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'brown', 'orange'];
+var colors = ['#d0e8de', '#cf808b', '#57374d', '#dec4a6', '#999999'];
 
-var xOrigin = 50;
-var yOrigin = 50;
+var xOrigin = 0;
+var yOrigin = 0;
 
-var allNeighbours = function(tile)
+var deltaI, deltaJ;
+
+
+function checkBounds(i,j)
+{
+	if(i < 0) return false;
+	if(j < 0) return false;
+	if(i >= numRows) return false;
+	if(j >= numColumns) return false;
+	return true;
+}
+
+var checkNeighbours = function(tile)
 {
 	var neighbours = [];
-	var minI = Math.max(0,tile.i-1);
-	var maxI = Math.min(numRows-1,tile.i+1);
-	var minJ = Math.max(0,tile.j-1);
-	var maxJ = Math.min(numColumns-1,tile.j+1);
-	for (var i = minI; i <= maxI; i++) {
-		for (var j = minJ; j <= maxJ; j++) {
-			if((i!= tile.i || j!=tile.j) && tile.color == grid[i][j].color)
-				neighbours.push(grid[i][j]);
+	for (var i = 0; i < deltaI.length; i++) {
+		var dI = deltaI[i];
+		var dJ = deltaJ[i];
+		if(checkBounds(tile.i + dI, tile.j + dJ))
+		{
+			var neighbour = grid[tile.i + dI][tile.j + dJ];
+			if(neighbour.color == tile.color)
+				neighbours.push(neighbour);
 		}
 	};
-	return neighbours;
-};
-
-var fourNeighbours = function(tile)
-{
-	var neighbours = [];
-	if(tile.i-1 >= 0 && tile.color == grid[tile.i-1][tile.j].color)
-		neighbours.push(grid[tile.i-1][tile.j]);
-	if(tile.i+1 < numRows && tile.color == grid[tile.i+1][tile.j].color)
-		neighbours.push(grid[tile.i+1][tile.j]);
-	if(tile.j-1 >= 0 && tile.color == grid[tile.i][tile.j-1].color)
-		neighbours.push(grid[tile.i][tile.j-1]);
-	if(tile.j+1 < numColumns && tile.color == grid[tile.i][tile.j+1].color)
-		neighbours.push(grid[tile.i][tile.j+1]);
 
 	return neighbours;
 };
 
-var neighbourFunctionIndex = 0;
-var neighbourFunctionArray = [fourNeighbours, allNeighbours];
 
 
 function getMousePos(evt) {
@@ -58,8 +55,8 @@ function getMousePos(evt) {
 }
 
 function getGridIndexesFromMouse(pos){
-	var i = Math.floor((pos.y - yOrigin)/tileSize);
-	var j = Math.floor((pos.x - xOrigin)/tileSize);
+	var i = Math.floor((pos.y - yOrigin)/tileHeight);
+	var j = Math.floor((pos.x - xOrigin)/tileWidth);
 	return {
 		i: i,
 		j: j
@@ -77,18 +74,45 @@ function onMouseClicked(event)
 	var indexes = getGridIndexesFromMouse(pos);
 	var visitedArray ={};
 	updateGrid(grid[indexes.i][indexes.j], visitedArray);
-	readjustGrid();
+	exportGridAsText();
 	draw();
+}
+
+function onColorSliderChanged(event)
+{
+	numberOfColors = Math.floor(event.target.value);
+}
+
+function onRowsSliderChanged(event)
+{
+	numRows = Math.floor(event.target.value);
+}
+
+function onColumnsSliderChanged(event)
+{
+	numColumns = Math.floor(event.target.value);
+}
+
+function onUpdateClicked(event)
+{
+	update();
+}
+
+
+
+function exportGridAsText()
+{
+	document.getElementById('textGrid').value = (toString(grid));
 }
 
 function updateGrid(tile, visitedArray)
 {
-	var neighbours = neighbourFunctionArray[neighbourFunctionIndex](tile);
+	var neighbours = checkNeighbours(tile);
 	grid[tile.i][tile.j].color = 'black';
+	grid[tile.i][tile.j].colorIndex = -1;
 	visitedArray[positionToIndex(tile.i,tile.j)] = true;
 	for (var i = 0; i < neighbours.length; i++) {
-			//neighbours[i].color = 'black';
-		if(visitedArray[positionToIndex(neighbours[i].i, neighbours[i].j)] != true)
+		if(!visitedArray[positionToIndex(neighbours[i].i, neighbours[i].j)])
 			updateGrid(neighbours[i], visitedArray);
 	};
 }
@@ -96,21 +120,22 @@ function updateGrid(tile, visitedArray)
 function getXYforIJ(i,j)
 {
 	return{
-		x: j*tileSize + xOrigin,
-		y: i*tileSize + yOrigin
+		x: j*tileWidth + xOrigin,
+		y: i*tileHeight + yOrigin
 	}
 }
 
 function readjustGrid()
 {
 	//update rows
-	var emptyColumns = {};
+	var emptyColumns = [];
 	for (var j = 0; j < numColumns; j++) {
+		emptyColumns.push(false);
 		var newColumnColors = [];
 		for (var i = 0; i <numRows; i++) {
 			if(grid[numRows - i - 1][j].color != 'black')
 			{
-				newColumnColors.push(grid[numRows - i - 1][j].color);
+				newColumnColors.push(grid[numRows - i - 1][j].colorIndex);
 			}		
 		}
 
@@ -119,12 +144,12 @@ function readjustGrid()
 
 		for (var i = 0; i < newColumnColors.length; i++) {
 			var xy = getXYforIJ(numRows - i - 1,j);
-			grid[numRows - i - 1][j] = new Tile(xy.x,xy.y,numRows - i - 1,j,newColumnColors[i]);
+			grid[numRows - i - 1][j] = new Tile(xy.x,xy.y,numRows - i - 1,j,colors[newColumnColors[i]],newColumnColors[i]);
 
 		};
 		for (var i = newColumnColors.length; i < numRows; i++) {
 			var xy = getXYforIJ(numRows - i - 1,j);
-			grid[numRows - i - 1][j] = new Tile(xy.x,xy.y,numRows - i - 1,j,'black');
+			grid[numRows - i - 1][j] = new Tile(xy.x,xy.y,numRows - i - 1,j,'black',-1);
 		};
 	}
 
@@ -132,34 +157,39 @@ function readjustGrid()
 	for (var i = 0; i <numRows; i++) {
 		var newRowColors = [];
 		for (var j = 0; j < numColumns; j++) {
-			if(!(j in emptyColumns))
+			
+			if(!(emptyColumns[j]))
 			{
-				newRowColors.push(grid[i][j].color);
+				newRowColors.push(grid[i][j].colorIndex);
 			}
 		}
 
 		for (var j = 0; j < newRowColors.length; j++) {
 			var xy = getXYforIJ(i,j);
-			grid[i][j] = new Tile(xy.x,xy.y,i,j,newRowColors[j]);
+			grid[i][j] = new Tile(xy.x,xy.y,i,j,colors[newRowColors[j]],newRowColors[j]);
 		};
 
 		for (var j = newRowColors.length; j < numColumns; j++) {
 			var xy = getXYforIJ(i,j);
-			grid[i][j] = new Tile(xy.x,xy.y,i,j,'black');
+			grid[i][j] = new Tile(xy.x,xy.y,i,j,'black',-1);
 		};
 	}
 }
 
-function load(canvas){
-	width = canvas.width;
-	height = canvas.height;
-	numRows = Math.floor((height - 2*yOrigin) / tileSize);
-	numColumns = Math.floor((width - 2*xOrigin) / tileSize);
-	currentContext = canvas.getContext("2d");
+function randNumber(max)
+{
+	return Math.floor(max*Math.random() + 1);
+}
+
+function update(){
+
+	tileWidth = width / numColumns;
+	tileHeight = height / numRows;
+
+	deltaI = [-1,1,0,0];
+	deltaJ = [0,0,-1,1];
 	
-	canvas.addEventListener("mousedown", onMouseClicked, false);
-	//document.getElementById("showOverlayInput").addEventListener("change", onShowOverlayChanged, false);
-	
+
 	grid = new Array(numRows);
 	for (var i = 0; i < grid.length; i++) {
 		grid[i] = new Array(numColumns);
@@ -170,14 +200,34 @@ function load(canvas){
 		for (var j = 0; j < grid[i].length; j++) {
 			var randColorIndex = Math.floor((Math.random() * numberOfColors));
 			var xy = getXYforIJ(i,j);
-			grid[i][j] = new Tile(xy.x,xy.y,i,j,colors[randColorIndex]);
+			grid[i][j] = new Tile(xy.x,xy.y,i,j,colors[randColorIndex],randColorIndex);
 		}
 	};
 
+
+	exportGridAsText();
 	requestAnimationFrame(draw);
 }
 
+function load(canvas){
+	width = canvas.width;
+	height = canvas.height;
+	
+	currentContext = canvas.getContext("2d");
+	
+	canvas.addEventListener("mousedown", onMouseClicked, false);
+	document.getElementById("colorSlider").addEventListener("change", onColorSliderChanged, false);
+	document.getElementById("rowsSlider").addEventListener("change", onRowsSliderChanged, false);
+	document.getElementById("columnsSlider").addEventListener("change", onColumnsSliderChanged, false);
+	document.getElementById("updateButton").addEventListener("click", onUpdateClicked, false);
+	
+	update();
+}
+
+
+
 function draw(){
+
 	currentContext.fillStyle = '#222';
 	currentContext.strokeStyle = 'white';
 	currentContext.fillRect(0,0,width,height);
@@ -189,17 +239,32 @@ function draw(){
 		}
 	}
 	
-	//requestAnimationFrame(draw);
 
 }
 
+function toString(grid)
+{
+	var string = "";
+	for (var i = 0; i < grid.length; i++) {
+		for (var j = 0; j < grid[i].length; j++) {
+			if(grid[i][j].color =='black')
+				string = string + ' ';
+			else
+				string = string + '*';
+		}
+		string = string + '\n';
+	}
+	return string;
+}
 
-function Tile(x,y,i,j,color){
+
+function Tile(x,y,i,j,color,colorIndex){
    this.x = x;
    this.y = y;
    this.i = i;
    this.j = j;
    this.color = color;
+   this.colorIndex = colorIndex;
 }
 
 Tile.prototype = {
@@ -207,12 +272,12 @@ Tile.prototype = {
         context.fillStyle = this.color;
 		context.strokeStyle = 'black';
 		context.lineWidth = 1;
-		context.fillRect(this.x, this.y, tileSize, tileSize);
-		context.strokeRect(this.x, this.y, tileSize, tileSize);
+		context.fillRect(this.x, this.y, tileWidth, tileHeight);
+		context.strokeRect(this.x, this.y, tileWidth, tileHeight);
 
     },
     clone: function(){
-    	return new Tile(this.x, this.y, this.i, this.j, this.color);
+    	return new Tile(this.x, this.y, this.i, this.j, this.color, this.colorIndex );
     }
 
 };
